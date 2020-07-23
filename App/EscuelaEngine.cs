53d5c8a6e0d50f4075229a3b14974f55104764ca
Adán García <adgarboc@gmail.com>
2020-07-23 16:09:54 -0500

    Codigo Final

---
+++ App/EscuelaEngine.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreEscuela.Entidades;
+
+namespace CoreEscuela
+{
+    public class EscuelaEngine
+    {
+        public Escuela Escuela { get; set; }
+
+        public EscuelaEngine()
+        {
+
+        }
+        public void Inicializar()
+        {
+            Escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
+                        ciudad: "Bogotá", pais: "Colombia"
+                        );
+            CargarCursos();
+            CargarAsignaturas();
+            CargarEvaluaciones();
+        }
+
+        private void CargarEvaluaciones()
+        {
+            int[] numEvaluacion = new int[] { 1,2,3,4,5 };
+            foreach (var curso in Escuela.Cursos)
+            {
+                var evaluaciones = from num in numEvaluacion
+                                 from alumno in curso.Alumnos
+                                 from asignatura in curso.Asignaturas
+                                 select new Evaluaciones {Nombre = $"Parcial {num}", Alumno = alumno, Asignatura = asignatura, Nota = GenerarNotaAlAzar(), };
+                curso.Evaluaciones = evaluaciones.ToList();
+            }
+        }
+        private float GenerarNotaAlAzar()
+        {
+            Random rnd = new Random();
+            float cantRandom = (float)rnd.Next(0,50) / 10;
+            return (float) Math.Round(cantRandom, 1);
+        }
+        private void CargarAsignaturas()
+        {
+            foreach (var curso in Escuela.Cursos)
+            {
+                List<Asignatura> listaAsignaturas = new List<Asignatura>(){
+                    new Asignatura{Nombre = "Matemáticas"},
+                    new Asignatura{Nombre = "Educación Física"},
+                    new Asignatura{Nombre = "Castellano"},
+                    new Asignatura{Nombre = "Ciencias Naturales"}
+                };
+                curso.Asignaturas = listaAsignaturas;
+            }
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar(int cantidad)
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+            return listaAlumnos.OrderBy((al) => al.UniqueId).Take(cantidad).ToList();
+        }
+
+        private void CargarCursos()
+        {
+            Escuela.Cursos = new List<Curso>()
+            {
+                new Curso(){Nombre = "101", Jornada = TiposJornada.Mañana},
+                new Curso(){Nombre = "201", Jornada = TiposJornada.Mañana},
+                new Curso{Nombre = "301", Jornada = TiposJornada.Mañana},
+                new Curso(){Nombre = "401", Jornada = TiposJornada.Tarde},
+                new Curso(){Nombre = "501", Jornada = TiposJornada.Tarde},
+            };
+
+            Random rnd = new Random();
+            foreach (var c in Escuela.Cursos)
+            {
+                int cantRandom = rnd.Next(5, 20);
+                c.Alumnos = GenerarAlumnosAlAzar(cantRandom);
+            }
+        }
+    }
+}
